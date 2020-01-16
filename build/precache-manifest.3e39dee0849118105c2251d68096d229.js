f016349e9168be102a28e39c57b4d210888cc1b0
BohdanKasian <psaiker12@gmail.com>
2020-01-16 18:13:22 +0200

    test in heroku

---
+++ build/precache-manifest.3e39dee0849118105c2251d68096d229.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1a74cda376cc5d541d9b74227937e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0768cc0da5dcaa03fdc2",
+    "url": "/static/css/2.4c487673.chunk.css"
+  },
+  {
+    "revision": "a2debb0a2f5cd63e6575",
+    "url": "/static/css/main.37042e7c.chunk.css"
+  },
+  {
+    "revision": "0768cc0da5dcaa03fdc2",
+    "url": "/static/js/2.3e1b60f9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3e1b60f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a2debb0a2f5cd63e6575",
+    "url": "/static/js/main.886d1392.chunk.js"
+  },
+  {
+    "revision": "150151168e0535f09a71",
+    "url": "/static/js/runtime-main.5fbf1490.js"
+  },
+  {
+    "revision": "01567a84df3d7991e344240ab14df218",
+    "url": "/static/media/dust-bin.01567a84.svg"
+  }
+]);
